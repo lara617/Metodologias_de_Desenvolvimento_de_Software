8db9652aebdfe9836aa3f042fa0abcea94e5d1e7
deisy321 <146828736+deisy321@users.noreply.github.com>
2024-01-16 14:40:27 +0000

    html

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ function toggleVisibility(id) {
       informacaoDiv.style.display = "none";
     }
   }
+
+
+  function toggleVisibility(elementId) {
+    var element = document.getElementById(elementId);
+    if (element.style.display === 'none') {
+        element.style.display = 'block';
+    } else {
+        element.style.display = 'none';
+    }
+}
